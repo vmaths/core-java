cde97a60c8c2819e0f4009060112ff78208a22eb
raj-abraham <rajesthundathil@gmail.com>
2014-09-11 17:17:49 -0500

    updated usage of abstract class and methods

--- src/main/java/com/vmstate/abstractmodifier/AbstractMain.java
+++ src/main/java/com/vmstate/abstractmodifier/AbstractMain.java
@@ -8,7 +8,23 @@ public class AbstractMain
 {
 	public static void main (String[] args)
 	{
-		//Animal a  = new Animal();// if the Animal class is abstract, we can not create new instances of the animal class directly		
-		Animal a = new Cow();
+
+		// Animal a  = new Animal(); // since the Animal class is abstract, we can not create new instances of the animal class directly		
+		Animal animalObj = new Cow();
+		
+		System.out.println("------------------------------------------------------------");		
+		// name() method is defined only in abstract (Animal) class as abstract method
+		animalObj.name();
+		
+		System.out.println("------------------------------------------------------------");
+		// eat() method is defined as abstract in Animal class. It should be defined as concrete method in Cow class
+		animalObj.eat();
+		
+		System.out.println("------------------------------------------------------------");
+		// sleep() class is defined as concrete in both Animal and Cow class. So calling sleep() method would be executed from concrete (Cow) class
+		animalObj.sleep();
+		
+
+
 	}
 }
