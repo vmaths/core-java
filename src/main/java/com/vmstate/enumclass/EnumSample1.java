76f23ebf7d17c163a579454d2723c535531607ec
raj-abraham <rajesthundathil@gmail.com>
2014-09-12 12:46:03 -0500

    EnumSample1 added to the enumclass package

---
+++ src/main/java/com/vmstate/enumclass/EnumSample1.java
@@ -0,0 +1,34 @@
+package com.vmstate.enumclass;
+/**
+ * This example demonstrates simple usage of enum class
+ * Java enum class restrict a variable to have only one of a predefined values
+ * Values in the enumerated list are called enums
+ * It reduces the number of bugs in the code
+ * @author Geo 
+ *
+ */
+
+public class EnumSample1 
+{
+	public enum Margin
+	
+	{
+		top,right, bottom, left
+	}
+	
+	public static void main(String[] args)
+	{
+		int count = 1;
+		
+		 for (Margin m : Margin.values())
+		{
+		System.out.println("Margin " + count++ + " = " +m);
+		
+		}
+		 
+	}
+	
+	
+}
+
+
