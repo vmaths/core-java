87b747be4ce9718f14a808f6c1aa709b9efe8a68
raj-abraham <rajesthundathil@gmail.com>
2014-09-23 10:27:11 -0500

    Updated SampleArrayList and CollectionDemo classes in the collectionframework package

---
+++ src/main/java/com/vmstate/collectionframework/CollectionDemo.java
@@ -0,0 +1,42 @@
+package com.vmstate.collectionframework;
+
+import java.util.*;
+
+public class CollectionDemo 
+{
+	public static void main(String[] args)
+	{
+	      ArrayList <String >a1 = new ArrayList<String>();
+	      a1.add("California");
+	      a1.add("Florida");
+	      a1.add("Kansas");
+	      System.out.println(" ArrayList Elements");
+	      System.out.print("\t" + a1);
+
+	      LinkedList<String> l1 = new LinkedList<String>();
+	      l1.add("Illinois");
+	      l1.add("Newyork");
+	      l1.add("New mexico");
+	      System.out.println();
+	      System.out.println(" LinkedList Elements");
+	      System.out.print("\t" + l1);
+
+	      HashSet<String> s1 = new HashSet<String>(); 
+	      s1.add("Alaska");
+	      s1.add("Arizona");
+	      s1.add("Georgia");
+	      System.out.println();
+	      System.out.println(" Set Elements");
+	      System.out.print("\t" + s1);
+
+	      HashMap<String, String> m1 = new HashMap<String, String>(); 
+	      m1.put("Texas", "Austin");
+	      m1.put("Oregon", "Salem");
+	      m1.put("California", "Sacremento");
+	      m1.put("Alaska", "Juneau");
+	      System.out.println();
+	      System.out.println(" Map Elements");
+	      System.out.print("\t" + m1);
+	   }
+	}
+
--- src/main/java/com/vmstate/collectionframework/SampleArrayList.java
+++ src/main/java/com/vmstate/collectionframework/SampleArrayList.java
@@ -9,76 +9,81 @@ import java.util.ArrayList;
  */
 public class SampleArrayList 
 {
-	public static void main(String[] args)
-	
-	{
-		// list1 is an ArrayList with datatype String
-		ArrayList<String> list1 = new ArrayList<String>();
-		
-		// use add() method to add elements to the list
-		list1.add("California");
-		list1.add("Oregon");
-		list1.add("Washington");
-		
-		System.out.println("Elements of List1: " +list1);
-		
-		// adding more elements to the list
-		list1.add("Alaska");
-		System.out.println("Elements of List1: " + list1);
-		
-		// use remove() method to remove elements from the list
-		// here element is removed from index 0 (first position)
-		list1.remove(0);
-		System.out.println("Elements of List1: " +list1);
-		
-		// use get() method to retrieve an element from the list
-		// here the element index one is returned
-		String state = list1.get(1);
-		System.out.println("The second state is " + state);
+ public static void main(String[] args)
+ 
+ {
+  // list1 is an ArrayList with datatype String
+  ArrayList<String> list1 = new ArrayList<String>();
+  
+  // use add() method to add elements to the list
+  list1.add("California");
+  list1.add("Oregon");
+  list1.add("Washington");
+  System.out.println("Elements of List1: ");
+  System.out.println("\t " +list1);
+  
+  // adding more elements to the list
+  list1.add("Alaska");
+  System.out.println("Elements of List1: ");
+  System.out.println("\t " + list1);
+  
+  // use remove() method to remove elements from the list
+  // here element is removed from index 0 (first position)
+  list1.remove(0);
+  System.out.println("Elements of List1: ");
+  System.out.println("\t " +list1);
+  
+  // use get() method to retrieve an element from the list
+  // here the element index one is returned
+  String state = list1.get(1);
+  System.out.println("The second state is " + state);
 
-		// use indexOf() method to get the position of an element in the list
-		// here the position of the element "Alaska" is returned
-		int pos = list1.indexOf("Alaska");
-		System.out.println("Alaska is at position " + pos);
-		
-		// use size() method retrieves total number of elements in the list
-		System.out.println("Size of list: " + list1.size());
-		
-		ArrayList<String> list2 = new ArrayList<String>();
-		list2.add("Texas");
-		list2.add("Illinois");
-		list2.add("Florida");
-		list2.add("Kansas");
-		System.out.println(list2);
-		
-		// contains() method returns true if this list contains the specified element.
-		boolean val=list2.contains("Texas");
-		if(val==true) System.out.println("The element Texas is present in the list");
-		else  System.out.println("The element Texas is not present in the list");
-			
-		val=list2.contains("Alaska");
-		if(val==true) System.out.println("The element Alaska is present in the list");
-		else  System.out.println("The element Alaska is not present in the list");
-		
-		// use addAll() method to add all the elements from list2 to list1
-		// this method appends all of the elements in the specified collection to the end of this list 
-		list1.addAll(list2);
-		System.out.println("Elements of List1: " + list1);
-		
-		// set() method replaces the element at the specified position in this list with the specified element.
-		list1.set(1, "California");
-		System.out.println("California is added as second element in list1") ;
-		System.out.println("Elements of List1: " + list1);
-		
-		// trimToSize() method trims the capacity of this ArrayList instance to be the list's current size. 
-		// an application can use this operation to minimize the storage of an ArrayList instance.
-		list1.trimToSize();
-		System.out.println("Number of Elements in list1, after trimToSize() : " + list1.size());	
-		
-		//clear() method removes all of the elements from this list.
-		list2.clear();
-		System.out.println("Number of Elements in list2, after clear()  : " +list2.size());
-		
-	}
+  // use indexOf() method to get the position of an element in the list
+  // here the position of the element "Alaska" is returned
+  int pos = list1.indexOf("Alaska");
+  System.out.println("Alaska is at position " + pos);
+  
+  // use size() method retrieves total number of elements in the list
+  System.out.println("Size of list: " + list1.size());
+  
+  ArrayList<String> list2 = new ArrayList<String>();
+  list2.add("Texas");
+  list2.add("Illinois");
+  list2.add("Florida");
+  list2.add("Kansas");
+  System.out.println("Elements of List2: ");
+  System.out.println("\t" +list2);
+  
+  // contains() method returns true if this list contains the specified element.
+  boolean val=list2.contains("Texas");
+  if(val==true) System.out.println("The element Texas is present in the list");
+  else  System.out.println("The element Texas is not present in the list");
+   
+  val=list2.contains("Alaska");
+  if(val==true) System.out.println("The element Alaska is present in the list");
+  else  System.out.println("The element Alaska is not present in the list");
+  
+  // use addAll() method to add all the elements from list2 to list1
+  // this method appends all of the elements in the specified collection to the end of this list 
+  list1.addAll(list2);
+  System.out.println("Elements of List1: " );
+  System.out.println("\t " + list1);
+  
+  // set() method replaces the element at the specified position in this list with the specified element.
+  list1.set(1, "California");
+  System.out.println("California is added as second element in list1") ;
+  System.out.println("Elements of List1: ");
+  System.out.println("\t " + list1);
+  
+  
+  // trimToSize() method trims the capacity of this ArrayList instance to be the list's current size. 
+  // an application can use this operation to minimize the storage of an ArrayList instance.
+  list1.trimToSize();
+  System.out.println("Number of Elements in list1, after trimToSize() : " + list1.size()); 
+  
+  //clear() method removes all of the elements from this list.
+  list2.clear();
+  System.out.println("Number of Elements in list2, after clear()  : " +list2.size());
+  
+ }
 }
-
