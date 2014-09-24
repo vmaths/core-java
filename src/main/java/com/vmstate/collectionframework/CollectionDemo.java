7d93eb48b7cf20cc1fd6ebf3fe28f7182862da7c
raj-abraham <rajesthundathil@gmail.com>
2014-09-24 11:35:39 -0500

    updated CollectionDemo and SetDemo classes under the collectionframework package

--- src/main/java/com/vmstate/collectionframework/CollectionDemo.java
+++ src/main/java/com/vmstate/collectionframework/CollectionDemo.java
@@ -9,14 +9,16 @@ public class CollectionDemo
 {
 	public static void main(String[] args)
 	{
-	      ArrayList <String >a1 = new ArrayList<String>();
+		  //ArrayList is a dynamic array implementaion
+	      List <String >a1 = new ArrayList<String>();
 	      a1.add("California");
 	      a1.add("Florida");
 	      a1.add("Kansas");
 	      System.out.println(" ArrayList Elements");
 	      System.out.print("\t" + a1);
 
-	      LinkedList<String> l1 = new LinkedList<String>();
+	      //
+	      List<String> l1 = new LinkedList<String>();
 	      l1.add("Illinois");
 	      l1.add("Newyork");
 	      l1.add("New mexico");
@@ -24,7 +26,8 @@ public class CollectionDemo
 	      System.out.println(" LinkedList Elements");
 	      System.out.print("\t" + l1);
 
-	      HashSet<String> s1 = new HashSet<String>(); 
+	      // A collection of elements that contains no duplicate elements
+	      Set<String> s1 = new HashSet<String>(); 
 	      s1.add("Alaska");
 	      s1.add("Arizona");
 	      s1.add("Georgia");
@@ -32,7 +35,7 @@ public class CollectionDemo
 	      System.out.println(" Set Elements");
 	      System.out.print("\t" + s1);
 
-	      HashMap<String, String> m1 = new HashMap<String, String>(); 
+	      Map<String, String> m1 = new HashMap<String, String>(); 
 	      m1.put("Texas", "Austin");
 	      m1.put("Oregon", "Salem");
 	      m1.put("California", "Sacremento");
---
+++ src/main/java/com/vmstate/collectionframework/SetDemo.java
@@ -0,0 +1,61 @@
+package com.vmstate.collectionframework;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+/** This example demonstrates set interface
+ * 
+ * @author Rajesh Abraham
+ * @author Geo Thomas
+ */
+public class SetDemo 
+{	
+  public static void main(String args[])
+  { 
+    
+     //HashSet is a collection of unique elements
+     Set<Integer> set = new HashSet<Integer>();
+           set.add(34);
+           set.add(22);
+           set.add(10);
+           set.add(60);
+           set.add(30);
+           set.add(22);
+        System.out.println("The elements of the set");
+        System.out.println("\t" +set);
+     
+        // contains(); used to check whether the specified element present in the list
+        //Returns true if a specified object is an element within the collection
+        boolean var = set.contains(40);
+        if (var==true)System.out.println("The set contains the element");
+        else System.out.println("The set do not contain the element");
+        
+        // size(); returns the number of elements in the collection
+        System.out.println("The number of elements in the list : " + set.size());
+        
+        // remove(); removes a specified object from the collection
+        set.remove(30);
+        System.out.println("The elements of the set after removal");
+        System.out.println("\t" +set);
+        
+        //TreeSet will return a sorted list of items
+        TreeSet sortedSet = new TreeSet<Integer>(set);
+        System.out.println("The sorted list is:");
+        System.out.println(sortedSet);
+
+        System.out.println("The First element of the set is: "+
+                          (Integer)sortedSet.first());
+        System.out.println("The last element of the set is: "+
+                        (Integer)sortedSet.last());
+       
+        //clear(); removes all objects from the collection
+        set.clear();
+        System.out.println("The number of elements in the list after clear : " + set.size());
+        
+       
+        
+     }
+}
+     
+     
+  
